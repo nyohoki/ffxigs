8038775d56d2336f876b257d5dac930b94a35cd2
nyohoki <nyohoki@gmail.com>
2023-07-18 12:40:30 -0700

    Create brd-mordant_rime.lua

---
+++ equip/brd-mordant_rime.lua
@@ -0,0 +1,15 @@
+sets.brd-mordant_rime = {
+	range = "Iron Ram Horn",
+	head = "Wayfarer Circlet",
+	neck = "Piper's Torque",
+	ear1 = "Aoidos' Earring",
+	ear2 = "Moonshade Earring",
+	body = "Wayfarer Robe",
+	hands = "Fili Manchettes",
+	ring1 = "Carb. Ring",
+	ring2 = "Carb. Ring",
+	back = "Mesmeric Cape",
+	waist = "Gleeman's Belt",
+	legs = "Fili Rhingrave",
+	feet = "Inyanga Crackows"
+}
