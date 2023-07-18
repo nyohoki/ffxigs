556112284a526399ad236b533d0b0bc29c56988c
nyohoki <nyohoki@gmail.com>
2023-07-18 09:13:53 -0700

    Create brd-idle.lua

---
+++ equip/brd-idle.lua
@@ -0,0 +1,14 @@
+sets.aftercast.idle = {
+	-- head = "Aoidos' Calot +1",
+	neck = "Twilight Torque",
+	-- ear1 = "",
+	-- ear2 = "",
+	-- body = "Fili Hongreline",
+	-- hands = "Fili Manchettes",
+	ring1 = "Defending Ring",
+	ring2 = "Shadow Ring",
+	back = "Shadow Mantle",
+	-- waist = "",
+	-- legs = "Fili Rhingrave",
+	feet = "Fili Cothurnes"
+}
