3bc563da288fe10d702a243bdf4f0297fb2f7da9
nyohoki <nyohoki@gmail.com>
2023-07-18 08:30:55 -0700

    Create song-midcast.lua

---
+++ equip/song-midcast.lua
@@ -0,0 +1,14 @@
+sets.song-midcast = {
+	head = "Aoidos' Calot +1",
+	neck = "Aoidos' Matiniee",		
+	ear1 = "Loquac. Earring",
+	-- ear2 = "",
+	body = "Fili Hongreline",
+	hands = "Fili Manchettes",
+	ring1 = "Prolix Ring",
+	ring2 = "Carb. Ring",
+	back = "Twilight Cape",
+	waist = "Embla Sash",
+	legs = "Fili Rhingrave",
+	feet = "Bihu Slippers +1"
+}
