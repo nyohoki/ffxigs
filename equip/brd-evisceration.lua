81417c13ce7ffb747b3f88139a6bcb3372977831
nyohoki <nyohoki@gmail.com>
2023-07-18 09:37:25 -0700

    Create brd-evisceration.lua

---
+++ equip/brd-evisceration.lua
@@ -0,0 +1,14 @@
+sets.brd-evisceration = {
+	head = "Aya. Zucchetto +2",
+	neck = "Rancor Collar",
+	ear1 = "Moonshade Earring",
+	ear2 = "Brutal Earring",
+	body = "Ayanmo Corazza +2",
+	hands = "Aya. Manopolas +2",
+	ring1 = "Ramuh Ring",
+	ring2 = "Ramuh Ring",
+	back = "Atheling Mantle",
+	waist = "Cuchulain's Belt",
+	legs = "Aya. Cosciales +2",
+	feet = "Aya. Gambieras  +2"
+}
