3eb7e722b76ee9a2105d49b51699d92c5ce4c713
nyohoki <nyohoki@gmail.com>
2023-07-19 08:54:07 -0700

    Create dnc_rudras_storm.lua

---
+++ equip/dnc_rudras_storm.lua
@@ -0,0 +1,14 @@
+sets.dnc_rudras_storm = {
+	ammo="Charis Feather",
+	head="Meghanada Visor",
+	neck="Shadow Gorget",
+	ear2="Brutal Earring",
+	body="Maghanada Cuirie",
+	hands="Meghanada Gloves",
+	ring1="Ramuh Ring",
+	ring2="Ramuh Ring",
+	back="Senuna's Mantle",
+	waist="Cuchulain's Belt",
+	legs="Meg. Chausses",
+	feet="Meg. Jambeaux"
+}
