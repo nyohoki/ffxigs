70ca07f3378cf6bcc89447dcd521720e48213291
nyohoki <nyohoki@gmail.com>
2023-07-19 09:12:21 -0700

    Create dnc_shark_bite.lua

---
+++ equip/dnc_shark_bite.lua
@@ -0,0 +1,14 @@
+sets.dnc_shark_bite = {
+	ammo = "Charis Feather",
+	head = "Meghanada Visor",
+	neck = "Rancor Collar",
+	ear2 = "Brutal Earring",
+	body = "Maghanada Cuirie",
+	hands = "Meghanada Gloves",
+	ring1 = "Ramuh Ring",
+	ring2 = "Ramuh Ring",
+	back = "Senuna's Mantle",
+	waist = "Cuchulain's Belt",
+	legs = "Meg. Chausses",
+	feet = "Meg. Jambeaux"
+}
