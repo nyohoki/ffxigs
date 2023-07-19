77181bf42a71c5007be503b4b74a772767fc4811
nyohoki <nyohoki@gmail.com>
2023-07-19 09:23:06 -0700

    Create dnc_engaged.lua

---
+++ equip/dnc_engaged.lua
@@ -0,0 +1,15 @@
+sets.dnc_engaged = {
+	ammo = "Charis Feather",
+	head = "Meghanada Visor",
+	neck = "Asperity Necklace",
+	ear1 = "Suppanomimi",
+	ear2 = "Digni. Earring",
+	body = "Meghanada Cuirie",
+	hands = "Meghanada Gloves",
+	ring1 = "Epona's Ring",
+	ring2 = "Rajas Ring",
+	back = "Toetapper Mantle",
+	waist = "Windbuffet Belt",
+	legs = "Meg. Chausses",
+	feet = "Meg. Jambeaux"
+}
