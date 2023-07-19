a35b8280d93a2de3af2b9b8beb6824e32ed6b827
nyohoki <nyohoki@gmail.com>
2023-07-19 13:24:58 -0700

    Create dnc_idle.lua

---
+++ equip/dnc_idle.lua
@@ -0,0 +1,14 @@
+sets.dnc_idle = {
+	-- head = "",
+	-- neck = "",
+	-- ear1 = "",
+	-- ear2 = "",
+	-- body = "",
+	-- hands = "",
+	-- ring1 = "",
+	-- ring2 = "",
+	-- back = "",
+	-- waist = "",
+	-- legs = "",
+	feet = "Tandava Crackows"
+}
